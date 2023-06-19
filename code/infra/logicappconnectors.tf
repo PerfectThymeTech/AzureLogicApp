4e8e068e1d785cd7b33ab7aaa7f4e4c5517589b1
Marvin Buss <marvin.buss@gmail.com>
2023-06-19 16:17:11 +0200

    lint

--- code/infra/logicappconnectors.tf
+++ code/infra/logicappconnectors.tf
@@ -21,22 +21,22 @@ data "azurerm_managed_api" "managed_api_arm" {
 # }
 
 resource "azapi_resource" "api_connection_arm" {
-  type = "Microsoft.Web/connections@2016-06-01"
+  type      = "Microsoft.Web/connections@2016-06-01"
   parent_id = azurerm_resource_group.app_rg.id
-  name = "${local.prefix}-api-arm001"
-  location = var.location
-  tags = var.tags
-  
+  name      = "${local.prefix}-api-arm001"
+  location  = var.location
+  tags      = var.tags
+
   body = jsonencode({
     properties = {
       api = {
-        name = data.azurerm_managed_api.managed_api_arm.name
+        name        = data.azurerm_managed_api.managed_api_arm.name
         displayName = "Azure Resource Manager"
         description = "Azure Resource Manager exposes the APIs to manage all of your Azure resources."
-        iconUri = "https://connectoricons-prod.azureedge.net/u/shgogna/globalperconnector-train1/1.0.1639.3310/${data.azurerm_managed_api.managed_api_arm.name}/icon.png"
-        brandColor = "#003056"
-        id = data.azurerm_managed_api.managed_api_arm.id
-        type = "Microsoft.Web/locations/managedApis"
+        iconUri     = "https://connectoricons-prod.azureedge.net/u/shgogna/globalperconnector-train1/1.0.1639.3310/${data.azurerm_managed_api.managed_api_arm.name}/icon.png"
+        brandColor  = "#003056"
+        id          = data.azurerm_managed_api.managed_api_arm.id
+        type        = "Microsoft.Web/locations/managedApis"
       }
       customParameterValues = {}
       # nonSecretParameterValues = {}
